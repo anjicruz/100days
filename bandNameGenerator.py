defb5714fb119c48e047232fbbe928e5301eacce
anjicruz <anji_cruz@msn.com>
2023-01-26 08:59:41 +0800

    bandNameGenerator 1/26/2023

---
+++ bandNameGenerator.py
@@ -0,0 +1,6 @@
+print("Welcome to the band name generator.")
+street = input("What is the name of the city you grew up in?\n")
+pet = input("What is your pets name?\n")
+print("Your band name could be " + street + " " + pet)
+
+
